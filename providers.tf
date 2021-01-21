1e45bd9ffc544802d5a4ee2fcc0c6e95de2b64dc
Bruno Terra <bterra@scalefactory.com>
2021-01-21 17:26:19 +0000

    cleanup

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "eu-west-1"
 }
-
-# terraform {
-#   backend "s3" {
-#     bucket = "snyk-codepipeline-terraform-state-450691171617-us-east-1-prod"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#     dynamodb_table = "CodePipelineTerraformStateLock-prod"
-#   }
-# }
