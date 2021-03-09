fa409e54b59937f4786080b5c8d6c36c2a39c223
David O'Rourke <david@scalefactory.com>
2021-03-09 14:59:30 +0000

    Rename main.tf to iam.tf

--- iam.tf
+++ iam.tf
@@ -5,30 +5,31 @@ resource aws_iam_role lambda_role {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
 
-# Attach basic policy
+# Attach basic AWS managed policy to lambda role
 resource aws_iam_role_policy_attachment AWSLambdaBasicExecution {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Attach dynamodb policy
-resource aws_iam_role_policy_attachment LambdaDynamoDB {
+# Attach DynamoDB policy to lambda role
+resource aws_iam_role_policy_attachment lambda_dynamodb {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.dynamodb_policy.arn
 }
 
+# Attach S3 policy to lambda role
+resource aws_iam_role_policy_attachment lambda_s3 {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+# Create DynamoDB policy
 resource aws_iam_policy dynamodb_policy {
   path   = "/"
   policy = data.aws_iam_policy_document.dynamodb.json
 }
 
-# Attach S3 Policy
-resource aws_iam_role_policy_attachment LambdaS3 {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.s3_policy.arn
-}
-
-
+# Create S3 policy
 resource aws_iam_policy s3_policy {
   path   = "/"
   policy = data.aws_iam_policy_document.s3.json
