d2dee5279e249057b51c03a1b9ef25a0f2cbbd4a
David O'Rourke <david@scalefactory.com>
2021-03-09 11:50:51 +0000

    Update Terraform for 0.14 and TF style guide

--- main.tf
+++ main.tf
@@ -1,120 +1,138 @@
-
 locals {
   lambda_name = "${var.name}_lambda"
 }
 
 ###########################
-resource "aws_dynamodb_table" "data_set" {
+resource aws_dynamodb_table data_set {
   name           = "${var.name}_data_set"
   hash_key       = "name"
   write_capacity = 1
   read_capacity  = 1
+
   attribute {
     name = "name"
     type = "S"
   }
 }
 
-resource "aws_s3_bucket" "data_source" {
+resource aws_s3_bucket data_source {
   bucket = "${var.name}-data-source"
 }
+
 # Create policy
 
 # Create role
-data "aws_iam_policy_document" "lambda-assume-role-policy" {
+data aws_iam_policy_document lambda_assume_role_policy {
   statement {
-    actions = ["sts:AssumeRole"]
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
 
     principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
+      type = "Service"
+
+      identifiers = [
+        "lambda.amazonaws.com",
+      ]
     }
   }
 }
 
 # Create role for Lambda
-resource "aws_iam_role" "lambda_role" {
+resource aws_iam_role lambda_role {
   name               = "${var.name}_lambda_role"
   path               = "/lambda/"
-  assume_role_policy = data.aws_iam_policy_document.lambda-assume-role-policy.json
-
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
 
 # Attach basic policy
-resource "aws_iam_role_policy_attachment" "AWSLambdaBasicExecution" {
+resource aws_iam_role_policy_attachment AWSLambdaBasicExecution {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 # Attach dynamodb policy
-resource "aws_iam_role_policy_attachment" "LambdaDynamoDB" {
+resource aws_iam_role_policy_attachment LambdaDynamoDB {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.dynamodb_policy.arn
 }
 
-resource "aws_iam_policy" "dynamodb_policy" {
+resource aws_iam_policy dynamodb_policy {
   path   = "/"
   policy = data.aws_iam_policy_document.dynamodb.json
 }
 
-data "aws_iam_policy_document" "dynamodb" {
+data aws_iam_policy_document dynamodb {
   statement {
+    effect = "Allow"
+
     actions = [
       "dynamodb:BatchGet*",
       "dynamodb:DescribeStream",
       "dynamodb:DescribeTable",
       "dynamodb:Get*",
       "dynamodb:Query",
-      "dynamodb:Scan"
+      "dynamodb:Scan",
     ]
+
     resources = [
-      aws_dynamodb_table.data_set.arn
+      aws_dynamodb_table.data_set.arn,
     ]
   }
 }
 
 # Attach S3 Policy
-resource "aws_iam_role_policy_attachment" "LambdaS3" {
+resource aws_iam_role_policy_attachment LambdaS3 {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.s3_policy.arn
 }
 
 
-resource "aws_iam_policy" "s3_policy" {
+resource aws_iam_policy s3_policy {
   path   = "/"
   policy = data.aws_iam_policy_document.s3.json
 }
 
-data "aws_iam_policy_document" "s3" {
+data aws_iam_policy_document s3 {
   statement {
+    effect = "Allow"
+
     actions = [
-      "s3:*Object"
+      "s3:*Object",
     ]
+
     resources = [
-      "${aws_s3_bucket.data_source.arn}/*"
+      "${aws_s3_bucket.data_source.arn}/*",
     ]
   }
 }
 
 ## Allow lambda call
-resource "aws_lambda_permission" "allow_bucket" {
+resource aws_lambda_permission allow_bucket {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = local.lambda_name 
+  function_name = local.lambda_name
   # aws_lambda_function.func.arn
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.data_source.arn
 }
 
-resource "aws_s3_bucket_notification" "bucket_notification" {
+resource aws_s3_bucket_notification bucket_notification {
   bucket = aws_s3_bucket.data_source.id
 
   lambda_function {
     lambda_function_arn = local.lambda_name
-    events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "AWSLogs/"
     filter_suffix       = ".log"
+
+    events = [
+      "s3:ObjectCreated:*",
+    ]
   }
 
-  depends_on = [aws_lambda_permission.allow_bucket]
+  depends_on = [
+    aws_lambda_permission.allow_bucket,
+  ]
 }
