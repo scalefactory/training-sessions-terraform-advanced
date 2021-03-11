8ad864dbf0e2abba499947fcf7fe3a705f236f98
David O'Rourke <david@scalefactory.com>
2021-03-11 15:44:11 +0000

    Add github provider

--- backend.tf
+++ backend.tf
@@ -11,5 +11,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.31.0"
     }
+
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.5.1"
+    }
   }
 }
