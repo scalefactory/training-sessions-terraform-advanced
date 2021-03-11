c7c6b05f47713ac4d2e40b26d50c068b4998d5e7
David O'Rourke <david@scalefactory.com>
2021-03-11 15:36:39 +0000

    Add archive provider

--- backend.tf
+++ backend.tf
@@ -1,7 +1,12 @@
 terraform {
-  required_version = "~> 0.14.7"
+  required_version = "~> 0.14.8"
 
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.1.0"
+    }
+
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.31.0"
