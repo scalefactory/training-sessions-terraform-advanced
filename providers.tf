15a7c2a55f98857367111f2a0e18975bc0d366a0
David O'Rourke <david@scalefactory.com>
2021-03-12 15:10:08 +0000

    Add new providers to providers.tf file

--- providers.tf
+++ providers.tf
@@ -1,3 +1,39 @@
+# Place a credentials file in the root of the Terraform repository in the
+# format of:
+#
+# [tf_adv_training]
+# aws_access_key_id=AKIAEXAMPLE
+# aws_secret_access_key=foobarEXAMPLE
+#
+# This is not how we recommend you use credentials for real work, we recommend
+# tools such as `aws-vault` to help manage your credentials.
+#
+# The %%ACCOUNT_ID%% here will want to be replaced with your account ID. You
+# can do this with the following once you know your account ID:
+#
+# sed -i 's/%%ACCOUNT_ID%%/123456789012/g' providers.tf
+#
+# Where 123456789012 is your real account ID.
 provider aws {
+  region                  = "eu-west-1"
+  profile                 = "tf_adv_training"
+  shared_credentials_file = "credentials"
+}
+
+provider aws {
+  alias  = "lambda_admin"
   region = "eu-west-1"
+
+  assume_role {
+    role_arn = "aws:arn:iam::%%ACCOUNT_ID%%:role/TfAdvLambdaAdmin"
+  }
+}
+
+provider aws {
+  alias  = "read_only"
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn = "aws:arn:iam::%%ACCOUNT_ID%%:role/TfAdvReadOnly"
+  }
 }
