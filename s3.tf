003175842d58e3a6cfdc0dcac6c4b145b73d1920
David O'Rourke <david@scalefactory.com>
2021-03-15 13:37:06 +0000

    s3: Add public access blocks

--- s3.tf
+++ s3.tf
@@ -22,3 +22,26 @@ resource aws_s3_bucket_notification bucket_notification {
     aws_lambda_permission.allow_bucket,
   ]
 }
+
+resource aws_s3_bucket_public_access_block block {
+  bucket                  = aws_s3_bucket.data_source.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource aws_s3_account_public_access_block block {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  # Only one set of public access block changes can be made at a time, so we
+  # depend on the one above here.
+  # This could be removed to introduce a race condition and possible errors
+  # from the API.
+  depends_on = [
+    aws_s3_bucket_public_access_block.block,
+  ]
+}
