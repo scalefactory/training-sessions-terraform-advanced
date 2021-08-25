022a98a0ec1ae2d7e4882a24dcd87c5da6aa6938
Tim Bannister <tim@scalefactory.com>
2021-08-25 16:06:12 +0100

    Fix provider for data look ups
    Folks will need extra data lookups for cross-account access

--- data.tf
+++ data.tf
@@ -1,11 +1,9 @@
 # Get our current region
 data aws_region current {
-  provider = aws.read_only
 }
 
 # Get our caller identity
 data aws_caller_identity current {
-  provider = aws.read_only
 }
 
 # Prepare a zip file for deployment to AWS Lambda
